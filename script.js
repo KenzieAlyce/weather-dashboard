ba59c59faecf9084d50dd3dfb5dce159b99daa56
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-21 20:31:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 
 
-
-var citySearch = document.getElementByID.val(citySearch)
-var cityResult = document.getElementByID(cityResult)
-var humidity = document.getElementById(humidity)
-var temp = document.getElementById(temp)
-var wind = document.getElementById(wind)
-var uv = document.getElementById(uv)
-var button = document.getElementById(button)
-
-
-var apiKey = "70d08f263f5c227482ef245478ef60b5"
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q={citySearch}&appid=" + apiKey;
+citySearch = document.getElementByID.val(citySearch);
+var cityResult = document.getElementByID(cityResult);
+var humidity = document.getElementById(humidity);
+var temp = document.getElementById(temp);
+var wind = document.getElementById(wind);
+var uv = document.getElementById(uv);
+var button = document.getElementById(button);
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+var apiKey = "70d08f263f5c227482ef245478ef60b5";
+var queryURL = "api.openweathermap.org/data/2.5/forecast?q={citySearch}&appid=" +  apiKey;
+
+
     
-  
-  
-    // Transfer content to HTML
+  $("button").click(function(){
     
-    
-      $(".cityResult").html("cityResult" + response.name);
+    $.ajax;
+    url: queryURL;
+      method: "GET"
+
+  .then();
+
+$(".city").html("cityResult" + response.name);
       $(".wind").text("Wind Speed: " + response.wind.speed);
       $(".humidity").text("Humidity: " + response.main.humidity);
-      //$(".uv").text ("UV: "+ response.uv.);//
+  });
+
+
       
       // Convert the temp to fahrenheit
-      //var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
       // add temp content to html
    
